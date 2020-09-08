fda22859ac3a679207ba8bae35b9b316e981eda3
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-08 13:06:03 +0200

    Updated state with dummy monsters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [monsters, setMonsters] = useState([
+    {
+      name: 'Frankenstein',
+      id: 1
+    },
+    {
+      name: 'Dracula',
+      id: 2
+    },
+    {
+      name: 'Zombie',
+      id: 1
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {monsters.map(monster => (
+        <h1>{monster.name}</h1>
+      ))}
     </div>
   );
 }
