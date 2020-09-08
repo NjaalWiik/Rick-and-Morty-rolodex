5db8c53615d6f843f0f2607f1e3b1feaa4fffa91
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-08 13:23:15 +0200

    Fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [monsters, setMonsters] = useState([
-    {
-      name: 'Frankenstein',
-      id: 1
-    },
-    {
-      name: 'Dracula',
-      id: 2
-    },
-    {
-      name: 'Zombie',
-      id: 1
-    }
-  ]);
+  const [monsters, setMonsters] = useState([]);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => setMonsters(users));
+  }, []);
   return (
     <div className='App'>
       {monsters.map(monster => (
