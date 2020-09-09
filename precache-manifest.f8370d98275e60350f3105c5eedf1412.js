2c8954bd0283375a6410458770e58439dafde977
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-09 10:56:45 +0200

    Updates

---
+++ precache-manifest.f8370d98275e60350f3105c5eedf1412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac8fb1829f4950dee3ea7f16f5704f5",
+    "url": "/Rick-and-Morty-rolodex/index.html"
+  },
+  {
+    "revision": "773baf960c5a7d27c722",
+    "url": "/Rick-and-Morty-rolodex/static/css/main.08dc4198.chunk.css"
+  },
+  {
+    "revision": "43cd1e4b3cd0a6e923d0",
+    "url": "/Rick-and-Morty-rolodex/static/js/2.51b5c0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rick-and-Morty-rolodex/static/js/2.51b5c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773baf960c5a7d27c722",
+    "url": "/Rick-and-Morty-rolodex/static/js/main.cddec552.chunk.js"
+  },
+  {
+    "revision": "e4d742f27219b5283fbe",
+    "url": "/Rick-and-Morty-rolodex/static/js/runtime-main.04ee44d4.js"
+  }
+]);
